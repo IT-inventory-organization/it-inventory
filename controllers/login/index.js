d685f3ada1618f0daea1f6dbc5a809cbc77c9f8e
Ahmad <ahmads1704@gmail.com>
2021-09-27 22:46:40 +0700

    add feature login

--- controllers/login/index.js
+++ controllers/login/index.js
@@ -1,7 +1,11 @@
 const { Op } = require("sequelize");
-const { body, validationResult } = require("express-validator");
+const { body } = require("express-validator");
 const { generateToken } = require("../../helper/jwt");
 const ErrorValidation = require("../../middlewares/ErrorValidation");
+const { checkHashText, createHashText } = require("../../helper/bcrypt");
+const User = require("../../database/models/user");
+const sequelize = require("../../configs/database");
+const { DataTypes } = require("sequelize");
 
 const validationBody = [
   body("email").isLength({ min: 1 }).withMessage("A email is required"),
@@ -9,18 +13,54 @@ const validationBody = [
 ];
 
 module.exports = (router) => {
-  router.post("/", validationBody, ErrorValidation, (req, res) => {
+  router.post("/", validationBody, ErrorValidation, async (req, res) => {
     // Params for generateToken()
     const body = {
       email: req.body.email,
       password: req.body.password,
     };
 
-    // result
-    const payload = {
-      token: generateToken(body),
-    };
+    const user = User(sequelize, DataTypes);
+    try {
+      const getUser = await user.findOne({
+        where: {
+          [Op.or]: [
+            { email: req.body.email },
+            { npwp: req.body.email },
+            { username: req.body.email },
+          ],
+        },
+      });
 
-    res.status(200).json(payload);
+      // if username, email and npwp don't exist
+      if (!getUser) {
+        return res.status(404).json({
+          status: false,
+          message: "Login failed, please check email or password!",
+          data: [],
+        });
+      }
+
+      const result = getUser.toJSON();
+      const checkPassword = checkHashText(result.password, req.body.password);
+      if (checkPassword) {
+        const payload = {
+          token: generateToken({ email: result.email, user_id: result.id }),
+          success: true,
+          message: "Login successfully!",
+        };
+        res.status(200).json(payload);
+      } else {
+        return res.status(401).json({
+          status: false,
+          message: "Login failed, please check email or password!",
+          data: [],
+        });
+      }
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ message: error.message, status: false, data: [] });
+    }
   });
 };
