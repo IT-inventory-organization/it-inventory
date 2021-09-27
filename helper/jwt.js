3a87281cb4fd28f0536d809361418e104749d765
Ahmad <41444348+ahmad17-fe@users.noreply.github.com>
2021-09-27 16:25:50 +0700

    change Get to get

--- helper/jwt.js
+++ helper/jwt.js
@@ -2,12 +2,12 @@ const jwt = require("jsonwebtoken");
 const CONFIG = require("../config.js");
 
 const generateToken = (payload) => {
-  const token = jwt.sign(payload, CONFIG.JWT_SECRET_KEY);
+  const token = jwt.sign(payload, CONFIG.get("JWT_SECRET_KEY"));
   return token;
 };
 
 const verifyToken = (token) => {
-  return jwt.verify(token, CONFIG.JWT_SECRET_KEY);
+  return jwt.verify(token, CONFIG.get("JWT_SECRET_KEY"));
 };
 
 module.exports = {
