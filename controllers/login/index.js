51222d7d5d75b09dca8759eb8931a75bd2bedde6
Ahmad <41444348+ahmad17-fe@users.noreply.github.com>
2021-09-27 16:29:12 +0700

    add middleware ErrorValidation

--- controllers/login/index.js
+++ controllers/login/index.js
@@ -1,17 +1,15 @@
 const { Op } = require("sequelize");
 const { body, validationResult } = require("express-validator");
 const { generateToken } = require("../../helper/jwt");
+const ErrorValidation = require("../../middlewares/ErrorValidation");
 
-const validationBody = [body("email").notEmpty(), body("password").notEmpty()];
+const validationBody = [
+  body("email").isLength({ min: 1 }).withMessage("A email is required"),
+  body("password").isLength({ min: 1 }).withMessage("A password is required"),
+];
 
 module.exports = (router) => {
-  router.post("/", validationBody, (req, res) => {
-    // Validation body
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-
+  router.post("/", validationBody, ErrorValidation, (req, res) => {
     // Params for generateToken()
     const body = {
       email: req.body.email,
