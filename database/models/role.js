6002c9f6108ff55f98b120ec1a2d605ea3c61b43
Ahmad <41444348+ahmad17-fe@users.noreply.github.com>
2021-09-28 10:39:25 +0700

    refactor role.js

--- database/models/role.js
+++ database/models/role.js
@@ -1,23 +1,35 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class Role extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  Role.init({
-    name: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Role',
-  });
-  return Role;
-};
+// const {
+//   Model
+// } = require('sequelize');
+// module.exports = (sequelize, DataTypes) => {
+//   class Role extends Model {
+//     /**
+//      * Helper method for defining associations.
+//      * This method is not a part of Sequelize lifecycle.
+//      * The `models/index` file will call this method automatically.
+//      */
+//     static associate(models) {
+//       // define association here
+//     }
+//   };
+//   Role.init({
+//     name: DataTypes.STRING
+//   }, {
+//     sequelize,
+//     modelName: 'Role',
+//   });
+//   return Role;
+// };
+const Sequelize = require('sequelize');
+const db = require('../../configs/database');
+
+const User = db.define('Roles', {
+  name: {
+    type: Sequelize.STRING
+  }
+}, {
+  tableName: 'Roles',
+});
+
+module.exports = User;
