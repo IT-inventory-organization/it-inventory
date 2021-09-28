e8ea67b0e1692252bf3fa4b8437586f72503205f
Ahmad <41444348+ahmad17-fe@users.noreply.github.com>
2021-09-28 10:22:32 +0700

    membuat function loginAction

--- controllers/login/index.js
+++ controllers/login/index.js
@@ -12,63 +12,65 @@ const validationBody = [
   body("password").isLength({ min: 1 }).withMessage("A password is required"),
 ];
 
-module.exports = (router) => {
-  router.post("/", validationBody, ErrorValidation, async (req, res) => {
-    const user = User(sequelize, DataTypes);
-    try {
-      const getUser = await user.findOne({
-        where: {
-          [Op.or]: [
-            { email: req.body.email },
-            { npwp: req.body.email },
-            { username: req.body.email },
-          ],
-        },
+const loginAction = async (req, res) => {
+  const user = User(sequelize, DataTypes);
+  try {
+    const getUser = await user.findOne({
+      where: {
+        [Op.or]: [
+          { email: req.body.email },
+          { npwp: req.body.email },
+          { username: req.body.email },
+        ],
+      },
+    });
+
+    // if username, email and npwp don't exist
+    if (!getUser) {
+      return res.status(404).json({
+        status: false,
+        message: "User not found!",
+        data: [],
       });
-
-      // if username, email and npwp don't exist
-      if (!getUser) {
-        return res.status(404).json({
-          status: false,
-          message: "User not found!",
-          data: [],
-        });
-      }
-
-      const result = getUser.toJSON();
-
-      // Check user is active
-      if (!result.is_active) {
-        return res.status(401).json({
-          status: false,
-          message: "User is no longer active!",
-          data: [],
-        });
-      }
-
-      // Check password
-      const checkPassword = checkHashText(result.password, req.body.password);
-      if (checkPassword) {
-        // if the password is correct
-        const payload = {
-          token: generateToken({ email: result.email, user_id: result.id }),
-          success: true,
-          message: "Login successfully!",
-        };
-        res.status(200).json(payload);
-      } else {
-        // if the password is not correct
-        return res.status(401).json({
-          status: false,
-          message: "Login failed, enter the correct password!",
-          data: [],
-        });
-      }
-    } catch (error) {
-      // if there is a system error
-      return res
-        .status(500)
-        .json({ message: error.message, status: false, data: [] });
     }
-  });
+
+    const result = getUser.toJSON();
+
+    // Check user is active
+    if (!result.is_active) {
+      return res.status(401).json({
+        status: false,
+        message: "User is no longer active!",
+        data: [],
+      });
+    }
+
+    // Check password
+    const checkPassword = checkHashText(result.password, req.body.password);
+    if (checkPassword) {
+      // if the password is correct
+      const payload = {
+        token: generateToken({ email: result.email, user_id: result.id }),
+        success: true,
+        message: "Login successfully!",
+      };
+      res.status(200).json(payload);
+    } else {
+      // if the password is not correct
+      return res.status(401).json({
+        status: false,
+        message: "Login failed, enter the correct password!",
+        data: [],
+      });
+    }
+  } catch (error) {
+    // if there is a system error
+    return res
+      .status(500)
+      .json({ message: error.message, status: false, data: [] });
+  }
+};
+
+module.exports = (router) => {
+  router.post("/", validationBody, ErrorValidation, loginAction);
 };
