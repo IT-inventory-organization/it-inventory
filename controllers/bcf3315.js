81a3e6b4f6171f2c00e0a30555be0d6810ea9909
Adiy03 <adiyogo3@gmail.com>
2021-12-15 16:29:20 +0700

    tambahKolomNomordanLampiranTabelFormBC3315

---
+++ controllers/bcf3315.js
@@ -0,0 +1,15 @@
+// const { bcf3315, Sequelize } = require("./../database/models/bcf3315")
+
+// const Op = Sequelize.Op;
+
+// let self = {};
+
+// self.getAll = async (req, res) => {
+//     try {
+//         let data = await bcf3315.findAll({
+//             attributes: [
+//                 "nama", "nomorPo", "tanggal",
+//             ]
+//         })
+//     }
+// }
